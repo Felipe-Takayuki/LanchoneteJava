2e5be3b4578e63d91031d53986c38276bcbf91e5
Felipe-Takayuki <felipe.takahashi@etec.sp.gov.br>
2023-11-21 12:06:59 -0300

    sql

---
+++ sql/database.sql
@@ -0,0 +1,33 @@
+create database Lanchonete;
+
+use Lanchonete;
+
+create table Clientes(  
+   cpf varchar(255) not null primary key, 
+   nome varchar(255) not null,
+   idade int not null,
+   telefone varchar(255) not null,
+);
+
+create table Lanches(
+    id int not null auto_increment primary key,
+    nome varchar(255) not null,
+    descricao varchar(255) not null, 
+    preco decimal (10,2) not null,
+);
+
+create table Bebidas(
+    id int not null auto_increment primary key,
+    nome varchar(255) not null,
+    preco decimal (10,2) not null,
+);
+
+create table Pedidos(
+   id int not null auto_increment primary key,
+   lancheID int,
+   bebidaID int,
+   clienteID varchar(255) not null,  
+   foreign key(lancheID) references Lanches(id),
+   foreign key(bebidaID) references Bebidas(id),
+   foreign key(clienteID) references Clientes(id),
+);
---
+++ sql/queries.sql
@@ -0,0 +1,11 @@
+/* Mostrar todos os clientes */
+select * from Clientes;
+
+/* Mostrar todos os Lanches */
+select * from Lanches;
+
+/* Mostrar todas as bebidas */
+select * from Bebidas;
+
+/* Mostrar todos os pedidos */
+select * from Pedidos;
