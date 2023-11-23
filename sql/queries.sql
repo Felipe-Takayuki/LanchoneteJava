6a3f7085c6780adf89196a569afdfb598f17152b
Felipe-Takayuki <felipe.takahashi@etec.sp.gov.br>
2023-11-23 12:31:39 -0300

    pedidos

--- sql/queries.sql
+++ sql/queries.sql
@@ -7,5 +7,7 @@ select * from Lanches;
 /* Mostrar todas as bebidas */
 select * from Bebidas;
 
-/* Mostrar todos os pedidos */
-select * from Pedidos;
+
+/* Mostrar os pedidos dos clientes com informações*/
+
+SELECT clientes.cpf, clientes.nome,   bebidas.nome as bebida,lanches.nome as lanche,lanches.preco as precoLanche,    bebidas.preco as precoBebida FROM clientes JOIN pedidos ON clientes.cpf = pedidos.clienteID JOIN lanches ON pedidos.lancheID = lanches.id JOIN bebidas ON pedidos.bebidaID = bebidas.id;
